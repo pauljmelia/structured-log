22c90e2481606a9fa92dbda78d43c4901cd533fb
Paul J. Melia <paul@travelogic.co.za>
2020-04-10 18:28:46 +0200

    exposing MessageTemplate

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-import './polyfills/objectAssign';
+import "./polyfills/objectAssign";
 
-export { LogEventLevel } from './logEvent';
-export { Logger } from './logger';
-export { ConsoleSink } from './consoleSink';
-export { BatchedSink } from './batchedSink';
-export { DynamicLevelSwitch } from './dynamicLevelSwitch';
+export { LogEventLevel } from "./logEvent";
+export { Logger } from "./logger";
+export { ConsoleSink } from "./consoleSink";
+export { BatchedSink } from "./batchedSink";
+export { DynamicLevelSwitch } from "./dynamicLevelSwitch";
+export { MessageTemplate } from "./messageTemplate";
 
-import { LoggerConfiguration } from './loggerConfiguration';
+import { LoggerConfiguration } from "./loggerConfiguration";
 
 export function configure() {
   return new LoggerConfiguration();
